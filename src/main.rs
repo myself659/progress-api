34a96197b48ed40442a2b681a808ab9b88a06452
myself659 <myself659@gmail.com>
2021-10-14 19:32:24 +0800

    iter

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::{time::Duration, thread::sleep};
 const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 
-fn  progress<T>(v: Vec<T>, f: fn(&T)-> ()){
+fn  progress<Iter>(iter: Iter, f: fn(Iter::Item)-> ())
+where  Iter: Iterator {
     let mut i:usize = 1;
-    let n: &T;
-    for n in v.iter() {
+
+    for n in iter {
         println!("{}{}", CLEAR, "*".repeat(i));
         i += 1;
         f(n);
@@ -16,10 +17,13 @@ fn expensive_calculation(_n: &i32) {
 
 fn main() {
     let v: Vec<i32> = vec![1,2,3];
-    let mut i: usize = 1;
-    for  n in  v.iter() {
-        println!("{} {}", CLEAR, "*".repeat(i));
-        i += 1;
-        expensive_calculation(n);
-    }
+    progress(v.iter(), expensive_calculation);
+
+    println!("----------");
+    use std::collections::HashSet;
+    let mut h = HashSet::new();
+    h.insert(0);
+    h.insert(1);
+    h.insert(3);
+    progress(h.iter(), expensive_calculation);
 }
