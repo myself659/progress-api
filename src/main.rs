3f71ea22041966fdf42b999c51b45d2519c4087f
myself659 <myself659@gmail.com>
2021-10-14 19:20:15 +0800

    generics

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use std::{time::Duration, thread::sleep};
 const CLEAR: &str = "\x1B[2J\x1B[1;1H";
+
+fn  progress<T>(v: Vec<T>, f: fn(&T)-> ()){
+    let mut i:usize = 1;
+    let n: &T;
+    for n in v.iter() {
+        println!("{}{}", CLEAR, "*".repeat(i));
+        i += 1;
+        f(n);
+    }
+}
 fn expensive_calculation(_n: &i32) {
     sleep(Duration::from_secs(1));
 }
