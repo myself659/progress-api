2867ccf9d0eb75f2cdaf48a6832961ff433c7525
myself659 <myself659@gmail.com>
2021-10-14 19:05:18 +0800

    show  basic  progress

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@ fn expensive_calculation(_n: &i32) {
 
 fn main() {
     let v: Vec<i32> = vec![1,2,3];
+    let mut i: usize = 0;
     for  n in  v.iter() {
+        println!("{}", "*".repeat(i));
+        i += 1;
         expensive_calculation(n);
     }
 }
