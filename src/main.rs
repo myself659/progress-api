f6f6d5e34c591cb3975b69cf77bd3a6d116b05d4
myself659 <myself659@gmail.com>
2021-10-14 19:10:05 +0800

    add clear to the same line

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::{time::Duration, thread::sleep};
-
+const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 fn expensive_calculation(_n: &i32) {
     sleep(Duration::from_secs(1));
 }
 
 fn main() {
     let v: Vec<i32> = vec![1,2,3];
-    let mut i: usize = 0;
+    let mut i: usize = 1;
     for  n in  v.iter() {
-        println!("{}", "*".repeat(i));
+        println!("{} {}", CLEAR, "*".repeat(i));
         i += 1;
         expensive_calculation(n);
     }
